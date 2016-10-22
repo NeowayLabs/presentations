ccb9853a5c2d6475044669bd9e0e6099a7c308d2
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-10-22 15:03:50 -0200

    Going on with the presentation

---
+++ golang/resilient-services-in-go/examples/timeout.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	//Create a context with a 1 second timeout
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		1*time.Second,
+	)
+	fmt.Printf("%T\n", ctx)
+	fmt.Printf("%T\n", cancel)
+	//Call cancel to avoid leaking resources until timeout fires
+	cancel()
+}
