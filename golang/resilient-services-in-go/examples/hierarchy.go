15de165601db90526f8376717eace4f6ba5a555c
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-11-02 15:20:25 -0200

    Dont block properly

--- golang/resilient-services-in-go/examples/hierarchy.go
+++ golang/resilient-services-in-go/examples/hierarchy.go
@@ -6,13 +6,17 @@ import (
 	"time"
 )
 
+func notDone(a <-chan struct{}, b <-chan struct{}) bool {
+	return a != nil || b != nil
+}
+
 func main() {
 	parent, parentCancel := context.WithTimeout(context.Background(), 10*time.Second)
 	child, childCancel := context.WithTimeout(parent, 5*time.Second)
 	defer parentCancel()
 	defer childCancel()
 	waitParent, waitChild := parent.Done(), child.Done()
-	for {
+	for notDone(waitParent, waitChild) {
 		select {
 		case <-waitParent:
 			fmt.Println("Parent done")
--- golang/resilient-services-in-go/examples/hierarchy3.go
+++ golang/resilient-services-in-go/examples/hierarchy3.go
@@ -6,13 +6,17 @@ import (
 	"time"
 )
 
+func notDone(a <-chan struct{}, b <-chan struct{}) bool {
+	return a != nil || b != nil
+}
+
 func main() {
 	parent, parentCancel := context.WithTimeout(context.Background(), 5*time.Second)
 	child, childCancel := context.WithTimeout(parent, 10*time.Second)
 	defer childCancel()
 	waitParent, waitChild := parent.Done(), child.Done()
 	go func() { parentCancel() }()
-	for {
+	for notDone(waitParent, waitChild) {
 		select {
 		case <-waitParent:
 			fmt.Println("Parent done")
