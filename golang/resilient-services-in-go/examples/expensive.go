9090c28cb7e2f7d6fc26c61d79cffd409056a783
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-11-02 15:55:59 -0200

    Add expensive ops explanation

--- golang/resilient-services-in-go/examples/expensive.go
+++ golang/resilient-services-in-go/examples/expensive.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func expensiveOp(ctx context.Context) {
+	for {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Still calculating nothing")
+	}
+}
 
 func main() {
-	fmt.Println("vim-go")
+	calcDone := make(chan struct{})
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	go func() {
+		expensiveOp(ctx)
+		calcDone <- struct{}{}
+	}()
+
+	select {
+	case <-calcDone:
+		fmt.Println("Calculation done")
+	case <-ctx.Done():
+		fmt.Println("Calculation Timeout")
+	}
 }
